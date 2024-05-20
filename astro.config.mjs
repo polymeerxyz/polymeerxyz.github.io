bc19cdcddd7018d6ccb2ccd78ee1b5945d3f8afb
Khai Nguyen <nguyenquangkhai2210@gmail.com>
2024-05-20 10:07:56 +0700

    feat: add homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,30 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://polymeer.xyz',
-	output: 'static',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				twitter: 'https://x.com/polymeer_xyz',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://polymeer.xyz',
+  output: 'static',
+  integrations: [starlight({
+    title: 'My Docs',
+    social: {
+      twitter: 'https://x.com/polymeer_xyz'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), tailwind()]
 });
---
+++ src/components/UI/home/Testimonial/data.ts
@@ -0,0 +1,108 @@
+export const sliderData = [
+    {
+        content: `I am impressed with Subwallet's intuitive design. Navigating
+        the interface was a breeze. Features are well-organized,
+        and the user experience is clearly at the heart of its
+        design, making asset management straightforward and efficient.
+        What sets Subwallet apart, however, is its dedication to
+        staying ahead of the curve.`,
+        name: "Muddleebee",
+        contact: "@muddleebee",
+        role: "Polkadot Ambassador"
+    },
+    {
+        content: `I really like the work that SubWallet has been doing, helping
+        new users onboard the Polkadot Ecosystem with an easy-to-use
+        and outstanding desktop extension UI, as well as the mobile
+        wallet. I love Dotinsights; it provides great information
+        that helps us better understand the direction of the entire
+        project.`,
+        name: "Maarmapa",
+        contact: "@maarmapa",
+        role: "Polkadot LATAM Head Ambassador"
+    },
+    {
+        content: `As a daily user of SubWallet, I can attest to its reliability,
+        ease of use, and overall impact on my workflow. Upon discovering
+        SubWallet, I was immediately impressed with their commitment
+        to advancing the blockchain space through a seamless, secure,
+        and user-friendly wallet solution.`,
+        name: "Marvin Tong",
+        contact: "@marvin_tong",
+        role: "Co-founder of Phala Network"
+    },
+    {
+        content: `As a person working in the blockchain industry, I am very
+        cautious with security and UX for any wallet interaction.
+        After finding SubWallet last year, it became my go-to non-custodial
+        wallet since then. I am impressed with its token-centric
+        focus, and continuous improvements the team has made.`,
+        name: "Justin Pham",
+        contact: "@justinphamnz",
+        role: "Co-founder of Bit.Country"
+    },
+    {
+        content: `SubWallet team expertise in building secure and user-friendly
+        wallet solutions has changed the way users interact with
+        Astar. With their innovative features and commitment to
+        user experience, SubWallet has become an indispensable
+        tool for seamless transactions and asset management.`,
+        name: "Maarten",
+        contact: "@henskensm",
+        role: "Head of Astar Foundation"
+    },
+    {
+        content: `SubWallet truly elevates the user experience within the
+        Polkadot ecosystem. The simplicity of cross-chain transfers
+        (XCM), comprehensive NFT support, straightforward crowdloan
+        participation, and intuitive staking processes make even
+        advanced technologies approachable.`,
+        name: "Dmitry Zhelezov",
+        contact: "@dmitry-zhelezov",
+        role: "Co-founder of Subsquid"
+    },
+    {
+        content: `SubWallet is a user-friendly and open-source wallet for
+        the Polkadot and Kusama ecosystem. It supports a wide range
+        of parachains and assets. With its intuitive interface
+        and commitment to open-source principles, SubWallet offers
+        a reliable option for those seeking a versatile and accessible
+        wallet.`,
+        name: "Bryan Chen",
+        contact: "@XiliangChen",
+        role: "Co-founder of Acala"
+    },
+    {
+        content: `Onboarding users to web3 products can be a challenge. Working
+        with SubWallet for the release of our first season of Awesome
+        Ajuna Avatars, made this a breeze! The combination of a
+        fluid user experience and focus on security makes SubWallet
+        an ideal partner for our AAA and more!`,
+        name: "Nicholas",
+        contact: "@nickdouz",
+        role: "Co-founder of Ajuna Network"
+    },
+    {
+        content: `Thanks to the xcm transfer feature of SubWallet, xcRHEE
+        can finally say goodbye to polkadot.js extension wallet,
+        which was supposed to be used by the developers in most
+        cases. Now, I no longer have to search for different applications
+        from different parachains interfaces when I want to cross-transfer
+        my assets!`,
+        name: "Sang Hyun Rhee",
+        contact: "@RHEE_UNION",
+        role: "Head of East Asia of Polkadot"
+    },
+    {
+        content: `I recommend SubWallet for three reasons. SubWallet has
+        a cool UI/UX design. The UI is intuitively navigable. Finally,
+        I trust the SubWallet team. They sincerely listen to user
+        feedback and provide quick updates. I have high expectations
+        for SubWallet's future evolution as a trusted "Crypto Wallet"
+        for users.`,
+        name: "Takeshi",
+        contact: "@tsarah0822",
+        role: "Astar Ambassador"
+    }
+ 
+] 
