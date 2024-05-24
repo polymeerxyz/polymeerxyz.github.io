aea023db072d642c8ec92cc87bb6afbc3f6c9932
nguyenquangkhai2210 <nguyenquangkhai2210@gmail.com>
2024-05-24 17:05:33 +0700

    feat: fix UI and add Icon lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,46 +2,40 @@ import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://polymeer.xyz",
   output: "static",
-  integrations: [
-    starlight({
-      title: "My Docs",
-      customCss: ["./src/tailwind.css"],
-      social: {
-        twitter: "https://x.com/polymeer_xyz",
-      },
-      sidebar: [
-        {
-          label: "Introduction",
-          autogenerate: {
-            directory: "introduction",
-          },
-        },
-        {
-          label: "Background",
-          autogenerate: {
-            directory: "reference",
-          },
-        },
-        {
-          label: "Knowledge Base",
-          autogenerate: {
-            directory: "knowledge-base",
-          },
-        },
-        {
-          label: "About Us",
-          autogenerate: {
-            directory: "about-us",
-          },
-        },
-      ],
-    }),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [starlight({
+    title: "My Docs",
+    customCss: ["./src/tailwind.css"],
+    social: {
+      twitter: "https://x.com/polymeer_xyz"
+    },
+    sidebar: [{
+      label: "Introduction",
+      autogenerate: {
+        directory: "introduction"
+      }
+    }, {
+      label: "Background",
+      autogenerate: {
+        directory: "reference"
+      }
+    }, {
+      label: "Knowledge Base",
+      autogenerate: {
+        directory: "knowledge-base"
+      }
+    }, {
+      label: "About Us",
+      autogenerate: {
+        directory: "about-us"
+      }
+    }]
+  }), tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
